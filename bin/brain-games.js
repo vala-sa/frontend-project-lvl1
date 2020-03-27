bf42e4ea6999c6e63f8c386186170f8736c5d65f
vala <manashka.vala@gmail.com>
2020-03-27 15:57:55 +0300

    + step3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log('Welcom to the Brain Games!');
+
+import greeting from '/home/vala/frontend-project-lvl1/src/index.js';
+
+console.log('Welcome to the Brain Games!');
+greeting();
+
+
